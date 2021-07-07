6ef030fac315b7d4691ec9a6bf093b40529b7d8b
Island <islandhuynh@gmail.com>
2021-07-07 03:35:31 -0400

    initialzied graph on pixela website

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import requests
+from requests.models import Response
 from params import user_params
 
 pixela_endpoint = "https://pixe.la/v1/users"
-response = requests.post(pixela_endpoint, json=user_params)
-print(response.text)
+
+# response = requests.post(pixela_endpoint, json=user_params)
+
+graph_endpoint = f"{pixela_endpoint}/{user_params['username']}/graphs"
+
+graph_config = {
+  "id": "graph1",
+  "name": "Studying Graph",
+  "unit": "hours",
+  "type": "float",
+  "color": "shibafu"
+}
+
+headers = {
+  "X-USER-TOKEN": user_params["token"]
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
