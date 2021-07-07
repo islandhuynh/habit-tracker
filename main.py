d1a69ddafcdcd7302e1c70a80d8f1d8c5c39577a
Island <islandhuynh@gmail.com>
2021-07-07 03:44:34 -0400

    Added post request to add pixel with quantity and current date

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import date
 from requests.models import Response
 from params import user_params
 
@@ -6,18 +7,27 @@ pixela_endpoint = "https://pixe.la/v1/users"
 
 # response = requests.post(pixela_endpoint, json=user_params)
 
-graph_endpoint = f"{pixela_endpoint}/{user_params['username']}/graphs"
+# graph_endpoint = f"{pixela_endpoint}/{user_params['username']}/graphs"
 
-graph_config = {
-  "id": "graph1",
-  "name": "Studying Graph",
-  "unit": "hours",
-  "type": "float",
-  "color": "shibafu"
-}
+# graph_config = {
+#   "id": "graph1",
+#   "name": "Studying Graph",
+#   "unit": "hours",
+#   "type": "float",
+#   "color": "shibafu"
+# }
 
 headers = {
   "X-USER-TOKEN": user_params["token"]
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+
+pixel_config = {
+  "date": date.today().strftime("%Y%m%d"),
+  "quantity": "5",
+}
+
+pixela_add_endpoint = f"{pixela_endpoint}/{user_params['username']}/graphs/graph1"
+response = requests.post(url=pixela_add_endpoint, json=pixel_config, headers=headers)
+print(response)
